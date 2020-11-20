c01bbbc620caf188fe60d6a36c39e51c1ae65cf8
srillia <srillia@163.com>
2020-11-20 15:48:43 +0800

    update

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -45,11 +45,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance       title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,          NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,          NULL,       1 << 8,       0,           -1 },
-	{ "Wine",     "tim.exe",     NULL,       0,            1,           -1 },
-	{ "Wine",     "wechat.exe",  NULL,       0,            1,           -1 },
+	/* class      instance       title       tags mask     isfloating   monitor    */
+	{ "Gimp",     NULL,          NULL,       0,            1,           -1,        },
+	{ "Firefox",  NULL,          NULL,       1 << 8,       0,           -1,        },
+	{ "Wine",     "tim.exe",     NULL,       0,            1,           -1,        },
+	{ "Wine",     "wechat.exe",  NULL,       0,            1,           -1,        },
 };
 
 /* layout(s) */
